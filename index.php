636d71ec73f0cee44d70ee6758d8c67277a24770
MohamadKaser <79741201+MohamadKaser@users.noreply.github.com>
2021-04-10 11:02:19 +0300

    I changed the index file so that it has form will action to data.php

--- index.php
+++ index.php
@@ -2,4 +2,39 @@
 
 echo 'Welcome to my site';
 
+
+
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Midterm</title>
+</head>
+<body><br><br>
+    
+    <form method='GET' action="data.php">
+        <label>Enter the Licence plat:
+            <input type="text" name='Licence_Plate'> 
+        </label>
+        <br>
+        <label>Enter the Brand:
+            <input type="text" name='Brand'> 
+        </label>
+        <br>
+        
+        <label>Enter the Model:
+            <input type="text" name='Model'> 
+        </label>
+        <br>
+        <label>Enter the Model_Year:
+            <input type="text" name='Model_Year'> 
+        </label><br>
+        <label>Enter the Color:
+            <input type="text" name='Color'> 
+        </label><br><br>
+        <input type="submit" value='Submit'>
+    </form>
+</body>
+</html>
